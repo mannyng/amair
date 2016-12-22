250455e9bd56c107686e3213350901e38c16322f
mannyng <emman.nwabueze@gmail.com>
2016-12-21 20:32:47 -0800

    english text dictionary

--- db/migrate/20161121074324_english_unaccented.rb
+++ db/migrate/20161121074324_english_unaccented.rb
@@ -1,8 +1,8 @@
 class EnglishUnaccented < ActiveRecord::Migration[5.0]
   def change
    execute %{
-    CREATE TEXT SEARCH CONFIGURATION english ( COPY = english );
-    ALTER TEXT SEARCH CONFIGURATION english ALTER MAPPING
+    CREATE TEXT SEARCH CONFIGURATION public.english ( COPY = pg_catalog.english );
+    ALTER TEXT SEARCH CONFIGURATION public.english ALTER MAPPING
     FOR hword, hword_part, word WITH unaccent, english_stem;
   }
   end
